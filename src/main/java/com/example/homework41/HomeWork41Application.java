2fd36e74e7d40b1c85e0b0e43fc713328aec6c29
Света <svetik24082@mail.ru>
2023-01-22 08:26:33 +0500

    1 коммит

---
+++ src/main/java/com/example/homework41/HomeWork41Application.java
@@ -0,0 +1,13 @@
+package com.example.homework41;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork41Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork41Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework41/HomeWork41ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork41ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
